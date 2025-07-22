6f157b40b230cecd4f89b40bfdaa827726872bc3
OLEKSII SYDORENKO <alohasid>
2025-07-22 13:19:04 +0300

    completed 3 tasks

--- src/components/FriendList/FriendList.tsx
+++ src/components/FriendList/FriendList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import FriendListItem from "./FriendsListItem.tsx";
+import FriendListItem from "../FriendsListItem/FriendsListItem.tsx";
 
 interface Friend {
     avatar: string;
